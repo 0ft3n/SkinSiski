4218f756f5d92ee963799e09e3ead978063bb5b4
_OfTeN_ <dkoften@gmail.com>
2021-12-14 22:47:06 +0300

    Initial commit

---
+++ test/tests.py
@@ -0,0 +1,101 @@
+import unittest
+import skin_siski
+import time
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_valid(self):
+        skin_siski.readNums("validTest.txt")
+        self.assertEqual(len(skin_siski.numbers), 6)
+
+    def test_min(self):
+        skin_siski.readNums("simpleTest.txt")
+        self.assertEqual(skin_siski.getMin(), -5)
+
+    def test_max(self):
+        skin_siski.readNums("simpleTest.txt")
+        self.assertEqual(skin_siski.getMax(), 324)
+
+    def test_1(self):
+        print("Проверка времени выполнения с файлом 1KB")
+        start_time = time.time()
+        skin_siski.readNums("simpleTest.txt")
+        print(f"Время чтения чисел из файла: {time.time() - start_time} секунд")
+        start_time = time.time()
+        skin_siski.getMin()
+        print(f"Время поиска минимума: {time.time() - start_time} секунд")
+        start_time = time.time()
+        skin_siski.getMax()
+        print(f"Время поиска максимума: {time.time() - start_time} секунд")
+        start_time = time.time()
+        skin_siski.getSum()
+        print(f"Время вычисления суммы: {time.time() - start_time} секунд")
+        start_time = time.time()
+        skin_siski.getMult()
+        print(f"Время вычисления произведения: {time.time() - start_time} секунд")
+        self.assertTrue(True, "sd")
+
+    def test_100(self):
+        print("Проверка времени выполнения с файлом 100KB")
+        start_time = time.time()
+        skin_siski.readNums("100.txt")
+        print(f"Время чтения чисел из файла: {time.time() - start_time} секунд")
+        start_time = time.time()
+        skin_siski.getMin()
+        print(f"Время поиска минимума: {time.time() - start_time} секунд")
+        start_time = time.time()
+        skin_siski.getMax()
+        print(f"Время поиска максимума: {time.time() - start_time} секунд")
+        start_time = time.time()
+        skin_siski.getSum()
+        print(f"Время вычисления суммы: {time.time() - start_time} секунд")
+        start_time = time.time()
+        skin_siski.getMult()
+        print(f"Время вычисления произведения: {time.time() - start_time} секунд")
+        print()
+        self.assertTrue(True)
+
+    def test_500(self):
+        print("Проверка времени выполнения с файлом 500KB")
+        start_time = time.time()
+        skin_siski.readNums("500.txt")
+        print(f"Время чтения чисел из файла: {time.time() - start_time} секунд")
+        start_time = time.time()
+        skin_siski.getMin()
+        print(f"Время поиска минимума: {time.time() - start_time} секунд")
+        start_time = time.time()
+        skin_siski.getMax()
+        print(f"Время поиска максимума: {time.time() - start_time} секунд")
+        start_time = time.time()
+        skin_siski.getSum()
+        print(f"Время вычисления суммы: {time.time() - start_time} секунд")
+        start_time = time.time()
+        skin_siski.getMult()
+        print(f"Время вычисления произведения: {time.time() - start_time} секунд")
+        print()
+        self.assertTrue(True)
+
+    def test_1000(self):
+        print("Проверка времени выполнения с файлом 1MB")
+        start_time = time.time()
+        skin_siski.readNums("1000.txt")
+        print(f"Время чтения чисел из файла: {time.time() - start_time} секунд")
+        start_time = time.time()
+        skin_siski.getMin()
+        print(f"Время поиска минимума: {time.time() - start_time} секунд")
+        start_time = time.time()
+        skin_siski.getMax()
+        print(f"Время поиска максимума: {time.time() - start_time} секунд")
+        start_time = time.time()
+        skin_siski.getSum()
+        print(f"Время вычисления суммы: {time.time() - start_time} секунд")
+        start_time = time.time()
+        skin_siski.getMult()
+        print(f"Время вычисления произведения: {time.time() - start_time} секунд")
+        print()
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
