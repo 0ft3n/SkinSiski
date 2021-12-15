cb1191fccca351570c30e620114eec4d1cdb66a4
0ft3n <dkoften@gmail.com>
2021-12-15 12:22:24 +0300

    Поднасрал

--- testMin.py
+++ testMin.py
@@ -7,7 +7,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_min(self):
         skin_siski.readNums("./simpleTest.txt")
-        self.assertEqual(skin_siski.getMin(), -5)
+        self.assertEqual(skin_siski.getMin(), -6)
         self.assertTrue(True)
 
 
